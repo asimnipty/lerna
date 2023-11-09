db4380897827272a7166c4776d89e64e00ade4eb
asimnipty <asimnipty@yahoo.com>
2023-11-09 20:17:13 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [add, setAdd] = useState(0);
+
+  useEffect (() => {
+    console.log( "COMPONENT MOUNT")
+  }, []);
+
+  useEffect (() => {
+    console.log( "COMPONENT UPDATE")
+  }, [add]);
+
+  // useEffect (() => {
+  //   return () => {
+  //     console.log( "COMPONENT UNMOUNT")
+  //   }
+  // }, [add])
+
+  return ( 
+    <div className='text-3xl text-center p-10'>
+      <h1 className='p-10 font-bold text-5xl text-green-800'>{add}</h1>
+      <div className=' space-x-2'>
+        <button onClick={() => setAdd(add + 1)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-md'>Add</button>
+        {/* <button onClick={() => setAdd(0)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-md'>RESET</button> */}
+      </div>     
     </div>
-  );
+  )
 }
 
 export default App;
